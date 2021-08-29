d899ace0d9a66f1dfcf11842d1d472b7a7b945d2
thekonly <joaopedro_m@icloud.com>
2021-08-29 18:33:22 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random()*100);
+
+let num3 = 100;
+let num4 = 6;
+const mod = num3 % num4;
+
+
+let numbers = (1,2,20);
+const max = Math.max(numbers);
